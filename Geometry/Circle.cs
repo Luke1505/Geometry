d6b2d69670edeccd30cc28473fdb7fa37feb14d9
Luke1550 <slodresko@gmail.com>
2022-10-24 18:16:15 +0200

    Circle class

--- Geometry/Circle.cs
+++ Geometry/Circle.cs
@@ -8,6 +8,47 @@ namespace Geometry
 {
     internal class Circle
     {
-        //a Circle class based on Points
+        private Point Center { get; set; }
+        private Point OuterPoint { get; set; }
+
+        public Circle(Point center, Point outerPoint)
+        {
+            Center = center;
+            OuterPoint = outerPoint;
+        }
+
+        public Circle()
+        {
+            Center = new Point();
+            OuterPoint = new Point();
+        }
+
+        public double Area()
+        {
+            double radius = Center.Distance(OuterPoint);
+            return Math.PI * radius * radius;
+        }
+
+        public double Circumference()
+        {
+            double radius = Center.Distance(OuterPoint);
+            return 2 * Math.PI * radius;
+        }
+
+        public double Diameter()
+        {
+            double radius = Center.Distance(OuterPoint);
+            return 2 * radius;
+        }
+
+        public double Radius()
+        {
+            return Center.Distance(OuterPoint);
+        }
+
+        public bool IsInside(Point point)
+        {
+            return Center.Distance(point) < Radius();
+        }
     }
 }
