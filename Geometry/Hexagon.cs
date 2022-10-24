c0b6ea119f4c3a7d18edbfa4d016df1f7b1bf662
Luke1550 <slodresko@gmail.com>
2022-10-24 18:21:08 +0200

    Adding Hexagon Class

---
+++ Geometry/Hexagon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    internal class Hexagon
+    {
+        //Hexagon Class based on Points
+        private Point one { get; set; }
+    }
+}
